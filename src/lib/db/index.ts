1366d601363a4b7d4e5d749eee96e9aae25dad78
Dhruv <dhruvchokshi005@gmail.com>
2026-04-26 23:51:33 -0400

    fix: use require for prisma client, skip lib check

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,18 +1,12 @@
 // sets up the prisma client as a singleton. one instance acorss the entire app. 
 
-import { PrismaClient } from "@prisma/client/edge"
-
-import { PrismaNeon } from "@prisma/adapter-neon"
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL!,
-})
-
-export const db = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+export const db = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
 
